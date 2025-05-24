ec0606f438edca65d186b7915fba5a4e9e2456b4
Abiskar Timsina <abiskartimsina4@gmail.com>
2025-05-25 02:34:18 +0545

    fix: remove invalid parser

--- lua/plugins/programming/treesitter.lua
+++ lua/plugins/programming/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "lua", "go", "python", "php", "ts" },
+            ensure_installed = { "lua", "go", "python", "php" },
             auto_install = false, -- This auto installs LSPs if not found but, just to keep stuff clean turning this off
             sync_install = false,
             highlight = { enable = true },
