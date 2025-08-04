b3bcb1edea37a8d8bdc017920cf72ad80aacdb2c
Abiskar Timsina <abiskar.timsina@grepsr.com>
2025-08-04 23:56:52 +0545

    feat: change formatter to ruff

--- lua/language_config/python3/lsp.lua
+++ lua/language_config/python3/lsp.lua
@@ -4,18 +4,35 @@ M.configure = function(lspconfig, capabilities)
     lspconfig.pyright.setup({
         capabilities = capabilities,
         settings = {
+            pyright = {
+                -- Using Ruff's import organizer
+                disableOrganizeImports = true,
+            },
             python = {
                 analysis = {
-                    typeCheckingMode = "off",
+                    typeCheckingMode = "strict",      -- NOTE: This may need to be `off` if it causes too much trouble.
+                    diagnosticMode = "openFilesOnly", -- optional: reduce overhead
                     diagnosticSeverityOverrides = {
                         reportMissingModuleSource = "none",
                         reportMissingImports = "error",
                         reportUndefinedVariable = "warning",
                     },
+                    -- Ignore all files for analysis to exclusively use Ruff for linting
+                    ignore = { '*' },
                 },
             },
         },
     })
+
+    lspconfig.ruff.setup {
+        capabilities = capabilities,
+        init_options = {
+            settings = {
+                -- Any extra CLI arguments for `ruff` go here.
+                args = {},
+            }
+        }
+    }
 end
 
 return M
--- lua/language_config/python3/none-ls.lua
+++ lua/language_config/python3/none-ls.lua
@@ -2,12 +2,12 @@
 -- https://github.com/nvimtools/none-ls-extras.nvim and flake8 was one of those builtins
 M = {}
 
-M.formatting = function (formatting)
-    return require("none-ls.formatting.autopep8")
-end
+-- M.formatting = function (formatting)
+-- return require("none-ls.formatting.ruff")
+-- end
 
-M.diagnostics = function (diagnostics)
-    return require("none-ls.diagnostics.flake8")
-end
+-- M.diagnostics = function (diagnostics)
+-- return require("none-ls.diagnostics.ruff")
+-- end
 
 return M
